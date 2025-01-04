8e90ea0097a05ced52f64c60960d918e1422dd44
Saar Shen <xiaomi7732@hotmail.com>
2025-01-04 15:53:05 -0800

    Compute the width of the levels

--- src/PrettyLogging.Console/LogLevelParser.cs
+++ src/PrettyLogging.Console/LogLevelParser.cs
@@ -7,20 +7,24 @@ namespace PrettyLogging.Console;
 internal sealed class LogLevelReverseParser
 {
     private LogLevelReverseParser() { }
+    public static IDictionary<LogLevel, string> _mapping = CreateMapping();
     public static LogLevelReverseParser Instance { get; } = new LogLevelReverseParser();
-
-    public static IDictionary<LogLevel, string> _mapping = new Dictionary<LogLevel, string>
-    {
-        [LogLevel.Critical] = "CRIT",
-        [LogLevel.Error] = "ERR",
-        [LogLevel.Warning] = "WARN",
-        [LogLevel.Information] = "INFO",
-        [LogLevel.Debug] = "DEBUG",
-        [LogLevel.Trace] = "TRACE",
-        [LogLevel.None] = "NONE",
-    };
 
     public string GetString(LogLevel logLevel) => _mapping[logLevel];
 
-    public int MaxWidth { get; } = "DEBUG".Length;
+    public int MaxWidth { get; } = _mapping.Values.Max(item => item.Length);
+
+    private static Dictionary<LogLevel, string> CreateMapping()
+    {
+        return new Dictionary<LogLevel, string>
+        {
+            [LogLevel.Critical] = "CRIT",
+            [LogLevel.Error] = "ERR",
+            [LogLevel.Warning] = "WARN",
+            [LogLevel.Information] = "INFO",
+            [LogLevel.Debug] = "DEBUG",
+            [LogLevel.Trace] = "TRACE",
+            [LogLevel.None] = "NONE",
+        };
+    }
 }
